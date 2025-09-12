51ead9ec19030835e08b9588438b222e619de0d4
BTS611 <gusmao.yasmin@escola.pr.gov.br>
2025-09-12 11:44:18 -0300

    update

--- dom_yas.js
+++ dom_yas.js
@@ -5,5 +5,5 @@
     rodri_circulo.style.borderadius = `${tamanho_rodri}px`
     rodri_circulo.style.width = `${tamanho_rodri}px`
     rodri_circul.style.helght = `${tamanho_rodri}px`
-    tamanho_rodri = tamanho_rodri+20 
+    tamanho_rodri = tamanho_rodri+20, 
 })
